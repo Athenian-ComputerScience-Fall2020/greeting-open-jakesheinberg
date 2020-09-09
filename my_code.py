772c78073216989e2ee55fddb3c967c496f44f6f
jakesheinberg <23jsheinberg@athenian.org>
2020-09-09 20:14:15 +0000

    Final greeting

--- my_code.py
+++ my_code.py
@@ -1,4 +1,12 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  none
 
 # Write a program that asks for the user's name and another piece of information.Then prints a response using both of the inputs.
+
+name=input("What is your name?")
+pets=input("How many pets do you have?")
+pets=int(pets)
+if pets==1:
+    print("Hello", name, "with", pets, "pet.")
+else:
+    print("Hello", name, "with", pets, "pets.")
